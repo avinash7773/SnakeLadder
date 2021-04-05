90248c71f45f7c17cd390edea0473af046fae7d7
avinash7773 <javinash228@gmail.com>
2021-04-05 18:07:13 +0530

    start from Zero

---
+++ snakeLadder.java
@@ -0,0 +1,11 @@
+class SnakeLadder{
+
+	void play(){
+	//	int dice_num = (int) Math.floor(Math.random() * 10) % 7;
+		System.out.println("Snake and Ladder game play with single player start at 0");
+	}
+	public static void main(String[] argv){
+	SnakeLadder obj = new SnakeLadder();
+	obj.play();
+	}
+}
