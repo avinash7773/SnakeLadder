5f3d6f864ee8ee29b90a51682ebf358715b1d51d
avinash7773 <javinash228@gmail.com>
2021-04-05 18:40:21 +0530

    player rolles the die and get number between 1 to 6

--- snakeLadder.java
+++ snakeLadder.java
@@ -1,8 +1,8 @@
 class SnakeLadder{
 
 	void play(){
-	//	int dice_num = (int) Math.floor(Math.random() * 10) % 7;
-		System.out.println("Snake and Ladder game play with single player start at 0");
+		int dice_num = (int) (Math.floor(Math.random() *10) % 6) +1;
+		System.out.println(dice_num);
 	}
 	public static void main(String[] argv){
 	SnakeLadder obj = new SnakeLadder();
